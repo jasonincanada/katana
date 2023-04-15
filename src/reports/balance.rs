a5c6414cbd5c37d09fd3b814608779beda1fd62e
jasonincanada <nirgle@gmail.com>
2023-04-15 09:43:44 -0400

    functional balance_changes()

--- src/reports/balance.rs
+++ src/reports/balance.rs
@@ -8,24 +8,25 @@ use crate::iterators::transactionsbymonth::transactions_by_month;
 // TODO: Assumes the same unit for all entries
 pub fn balance_changes(journal: &Journal) -> MonthGrid<Account, Amount> {
     let summary = JournalSummary::from(journal);
-    let mut grid = MonthGrid::new(summary.first_month, summary.final_month);
 
-    for (month, ts) in transactions_by_month(journal) {
-        let mut by_account: HashMap<Account, Amount> = HashMap::new();
-
-        for transaction in ts.iter() {
-            for entry in &transaction.entries {
-                by_account
-                    .entry(entry.account.clone())
-                    .and_modify(|amount| amount.add(&entry.amount))
-                    .or_insert_with(|| entry.amount.clone());
+    transactions_by_month(journal)
+        .into_iter()
+        .map(|(month, ts)| {
+            let by_account = ts.iter()
+                .flat_map(|transaction| &transaction.entries)
+                .map(|entry| (entry.account.clone(), entry.amount.clone()))
+                .fold(HashMap::<Account,Amount>::new(), |mut map, (account, amount)| {
+                    map.entry(account)
+                        .and_modify(|existing| existing.add(&amount))
+                        .or_insert(amount);
+                    map
+                });
+            (month, by_account)
+        })
+        .fold(MonthGrid::new(summary.first_month, summary.final_month), |mut grid, (month, by_account)| {
+            for (account, amount) in by_account {
+                grid.insert(account, month, amount);
             }
-        }
-
-        for (account, amount) in by_account {
-            grid.insert(account, month, amount);
-        }
-    }
-
-    grid
+            grid
+        })
 }
